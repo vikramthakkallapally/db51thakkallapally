c65010c4db8cc5d9b8e5a99c1660c728798aa177
Vikram Thakkallapally <89488488+vikramthakkallapally@users.noreply.github.com>
2021-11-12 12:38:03 -0600

    added id to url

--- controllers/tree.js
+++ controllers/tree.js
@@ -28,9 +28,25 @@ exports.tree_delete = function(req, res) {
     res.send('NOT IMPLEMENTED: Tree delete DELETE ' + req.params.id); 
 }; 
  
-// Handle Tree update form on PUT. 
-exports.tree_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Tree update PUT' + req.params.id); 
+// Handle Tree update form on PUT.
+exports.tree_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await tree.findById( req.params.id)
+// Do updates of properties
+if(req.body.treeColor)
+toUpdate.treeColor = req.body.treeColor;
+if(req.body.age) toUpdate.age = req.body.age;
+if(req.body.name) toUpdate.name = req.body.name;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
 
 exports.tree_view_all_Page = async function(req, res) { 
@@ -66,4 +82,16 @@ exports.tree_create_post = async function(req, res) {
         res.status(500); 
         res.send(`{"error": ${err}}`); 
     }   
+};
+
+// for a specific Tree.
+exports.tree_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await tree.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
